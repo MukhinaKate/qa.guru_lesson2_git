f3335f4bdb33092db9fe3d18751c6bcd6a98f357
Ekaterina Mukhina <56156830+MukhinaKate@users.noreply.github.com>
2024-11-26 20:26:08 +0300

    Update Main.java

--- src/main/java/com/ekaterina/dmitrievna96/Main.java
+++ src/main/java/com/ekaterina/dmitrievna96/Main.java
@@ -2,6 +2,6 @@ package com.ekaterina.dmitrievna96;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 }
